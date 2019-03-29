85f89aa97a9c8f4aa3d050ef2988ab9cbe3e3f4c
Stanislav Hordiyenko <stanislav.hordiyenko@gmail.com>
2019-03-29 23:41:15 +1100

    Add scrypt browser library

--- index.js
+++ index.js
@@ -71,3 +71,74 @@ function unlockPassword(masterPassword, url, passwordLength, key, capital, digit
 
     return password;
 }
+
+$(document).ready(function() {
+    var clipboard = new ClipboardJS('.copy.button');
+
+    clipboard.on('success', function(e) {
+        $('.ui.result.input').transition('pulse');
+        // e.clearSelection();
+    });
+
+    $('.ui.checkbox').checkbox();
+
+    $('#onlyDigits').click(function() {
+        if ($(this).checkbox("is checked") == true) {
+            $("#digit").checkbox('set disabled');
+            $("#specialCharacter").checkbox('set disabled');
+            $("#capital").checkbox('set disabled');
+        } else {
+            $("#digit").checkbox('set enabled');
+            $("#specialCharacter").checkbox('set enabled');
+            $("#capital").checkbox('set enabled');
+        }
+        
+    });
+
+    $('.message .close').on('click', function() {
+        $(this)
+            .closest('.message')
+            .transition('fade');
+        $('.tiny.message').remove();
+    });
+
+    $('div.checkbox.key').click(function() {
+        $('div.key.field').toggleClass('disabled');
+    });
+
+    $('div.buy.pro').click(function() {
+        ga('send', 'event', 'purchase', 'pro')
+        $('div.buy.pro').addClass('disabled');
+    });
+
+    $('div.submit.button').click(function() {
+        var masterPassword = $("input.master.password").val();
+        var websiteUrl = $('input[name="websiteUrl"').val();
+        var passwordLength = $('input[name="passwordLength"').val();
+        var key = $('input[name="key"').val();
+
+        var capital = $('#capital').checkbox("is checked");
+        var digit = $('#digit').checkbox("is checked");
+        var specialCharacter = $('#specialCharacter').checkbox("is checked");
+        var onlyDigits = $('#onlyDigits').checkbox("is checked");
+
+        $('div.submit.button').addClass('loading');
+
+        setTimeout(function() {
+            scrypt_module_factory(function (scrypt) {
+                var key = scrypt.crypto_scrypt(scrypt.encode_utf8("pleaseletmein"),
+                            scrypt.encode_utf8("SodiumChloride"),
+                            16384, 8, 30, 256);  // n, r, p <- which params to choose??
+                var unlockedPassword = base58Encode(scrypt.to_hex(key));
+                $('input.unlocked.password').val(unlockedPassword);
+            });
+
+            $('div.submit.button').removeClass('loading');
+        
+
+            // var unlockedPassword = unlockPassword(masterPassword, websiteUrl, passwordLength, key, capital, digit, specialCharacter, onlyDigits);
+
+            $('div.password.result').toggle();
+        }, 1000);
+    });
+});
